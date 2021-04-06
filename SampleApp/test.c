8dab2e38e69a3c5f2370ad50bf1c0892beecfc43
Patrícia Faria <63451914+hlolly@users.noreply.github.com>
2021-04-06 16:07:40 +0200

    Add files via upload

---
+++ SampleApp/test.c
@@ -0,0 +1,13 @@
+/*
+Author: Patricia Faria
+Purpose: This program prints out my name to the screen
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    // this code is displaying my name
+    printf("Hi, my name is Patricia");
+    return 0;
+}
